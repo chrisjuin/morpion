2903e96fcb163af1fefe845c9b35d5d47c5baf29
chrisjuin <c.juin.cj@gmail.com>
2017-08-03 16:10:52 +0200

    ajout des fichier

---
+++ app.js
@@ -0,0 +1,15 @@
+var image; 
+image= 0;
+
+$(".game").click(function(){
+
+	if (image%2 === 0) {
+		$(this).html("<img src='image1.png'>");
+	}else {
+
+		$(this).html("<img src='image2.png'>");
+	}
+
+image++;
+
+});
